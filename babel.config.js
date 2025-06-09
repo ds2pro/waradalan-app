e31f6be867e2df5a2b5507f35909e64dac547899
Hussein Fakih <admin@ds2pro.com>
2025-06-09 08:51:35 -0400

    Add cleaning steps to Github workflows

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"], // Required for drawer animations
+    plugins: [
+      ["react-native-reanimated/plugin"],
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./",
+          },
+        },
+      ],
+    ], // Required for drawer animations
   };
 };
